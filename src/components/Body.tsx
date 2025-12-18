7226ce8c5ca512f3927f81f13484adca542662bb
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-12-18 14:50:48 -0500

    UPDATE: Integrate Sounds into <Body /> and <Icon />

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,6 +1,7 @@
 import { DrumsProvider } from '@/context/Drums';
 import { EditingProvider } from '@/context/Editing';
 import { IsPlayingProvider } from '@/context/IsPlaying';
+import { SoundsProvider } from '@/context/Sounds';
 import VisualDisplay from './displays/VisualDisplay';
 import BeatSelector from './files/BeatSelector';
 import IO from './files/IO';
@@ -10,14 +11,16 @@ function Body() {
 	return (
 		<DrumsProvider>
 			<IsPlayingProvider>
-				<main className='Body flex flex-col justify-between gap-4 w-full max-w-screen-xl p-4 mx-auto'>
-					<EditingProvider>
-						<VisualDisplay />
-					</EditingProvider>
-					<Metronome />
-					<BeatSelector />
-					<IO />
-				</main>
+				<SoundsProvider>
+					<main className='Body flex flex-col justify-between gap-4 w-full max-w-screen-xl p-4 mx-auto'>
+						<EditingProvider>
+							<VisualDisplay />
+						</EditingProvider>
+						<Metronome />
+						<BeatSelector />
+						<IO />
+					</main>
+				</SoundsProvider>
 			</IsPlayingProvider>
 		</DrumsProvider>
 	);
--- src/components/displays/Icon.tsx
+++ src/components/displays/Icon.tsx
@@ -1,13 +1,19 @@
+import { useSounds } from '@/context/useSounds';
+import type { drums } from '@/types/drums';
+
 type IconProps = {
-	icon: 'cymbal' | 'snare' | 'bass';
+	icon: drums;
 	isActive?: boolean;
 };
 
 function Icon({ icon, isActive = false }: IconProps) {
+	const { playSound } = useSounds();
+
 	return (
 		<button
 			type='button'
 			className={`Icon ${icon} ${isActive ? 'active' : ''}`}
+			onClick={() => playSound(icon)}
 		>
 			<img src={`/icons/${icon}.png`} alt={icon} />
 		</button>
