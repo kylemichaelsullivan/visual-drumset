c470104e09fdb5bd37b30977a9af1b5a72602165
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-12-17 23:45:39 -0500

    UPDATE: Styles & Scripts

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,9 +1,9 @@
 export function getBackgroundClass(j: number) {
-	const bgClass = ["bg-gray-300", "bg-gray-100", "bg-gray-200", "bg-gray-100"];
+	const bgClass = ['bg-gray-300', 'bg-gray-100', 'bg-gray-200', 'bg-gray-100'];
 	return bgClass[j % 4];
 }
 
 export function getSubdivision(j: number): string {
-	const subdivisions = ["", "-e", "-&", "-a"];
+	const subdivisions = ['', '-e', '-&', '-a'];
 	return subdivisions[j];
 }
