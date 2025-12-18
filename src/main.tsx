cac93b77dd6cda5896cca227338e8b559dbb47a9
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-12-17 20:32:56 -0500

    FIX: main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App.tsx";
+import "./index.css";
 
-// biome-ignore lint/style/noNonNullAssertion: <explanation>
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+	ReactDOM.createRoot(rootElement).render(
+		<StrictMode>
+			<App />
+		</StrictMode>,
+	);
+}
