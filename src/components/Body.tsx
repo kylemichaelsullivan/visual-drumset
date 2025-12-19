38928a56099ecdbeaf1d53b1d53dd51807bd6635
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-12-19 00:35:43 -0500

    REFACTOR: Extract SoundsProvider

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,7 +1,7 @@
 import { DrumsProvider } from '@/context/Drums';
 import { EditingProvider } from '@/context/Editing';
 import { IsPlayingProvider } from '@/context/IsPlaying';
-import { SoundsProvider } from '@/context/Sounds';
+import { SoundsProvider } from '@/context/SoundsProvider';
 import VisualDisplay from './displays/VisualDisplay';
 import BeatSelector from './files/BeatSelector';
 import IO from './files/IO';
@@ -11,16 +11,16 @@ function Body() {
 	return (
 		<DrumsProvider>
 			<IsPlayingProvider>
-				<SoundsProvider>
-					<main className='Body flex flex-col justify-between gap-4 w-full max-w-screen-xl p-4 mx-auto'>
-						<EditingProvider>
+				<EditingProvider>
+					<SoundsProvider>
+						<main className='Body flex flex-col justify-between gap-4 w-full max-w-screen-xl p-4 mx-auto'>
 							<VisualDisplay />
-						</EditingProvider>
-						<Metronome />
-						<BeatSelector />
-						<IO />
-					</main>
-				</SoundsProvider>
+							<Metronome />
+							<BeatSelector />
+							<IO />
+						</main>
+					</SoundsProvider>
+				</EditingProvider>
 			</IsPlayingProvider>
 		</DrumsProvider>
 	);
