393042b95ed75d828305c19305e53899c04d7041
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-12-18 20:34:05 -0500

    UPDATE: Title for <Icon />

--- src/components/displays/Icon.tsx
+++ src/components/displays/Icon.tsx
@@ -9,10 +9,13 @@ type IconProps = {
 function Icon({ icon, isActive = false }: IconProps) {
 	const { playSound } = useSounds();
 
+	const title = icon.charAt(0).toUpperCase() + icon.slice(1);
+
 	return (
 		<button
 			type='button'
-			className={`Icon ${icon} ${isActive ? 'active' : ''}`}
+			className={`Icon ${title} ${isActive ? 'active' : ''}`}
+			title={title}
 			onClick={() => playSound(icon)}
 		>
 			<img src={`/icons/${icon}.png`} alt={icon} />
