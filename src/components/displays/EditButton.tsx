c0c1991d106b3efa845fef69dad819e9fcaf4c76
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2025-12-19 00:39:55 -0500

    REFACTOR: Move <EditButton />

--- src/components/displays/EditButton.tsx
+++ src/components/displays/EditButton.tsx
@@ -8,7 +8,7 @@ function EditButton() {
 	return (
 		<button
 			type='button'
-			className='absolute w-8 h-8 top-0 right-0 hover:ring-1'
+			className='EditButton absolute w-8 h-8 top-0 right-0 hover:ring-1'
 			title={isEditing ? 'See Beat' : 'Edit Beat'}
 			onMouseDown={() => setIsEditing(() => !isEditing)}
 		>
